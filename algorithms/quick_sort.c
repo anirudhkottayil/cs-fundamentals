a51053d54e7f86cd9cda79b1f7d9fe09fab71d38
Anirudh Kottayil Prince <anirudhkottayil@gmail.com>
2026-06-29 01:59:10 +1000

    feat: made efficient now O(nlogn) avg instead of O(n^2logn) avg

--- algorithms/quick_sort.c
+++ algorithms/quick_sort.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void move(int* arr, int length){
-  int temp = arr[0];
-  for (int i = 0; i < length-1; i++){
-    arr[i] = arr[i+1];
-  }
-  arr[length-1] = temp;
+void move(int* arr,int i, int length, int pivot, int pivot_index){
+  if (i == pivot_index-1){
+    int temp = arr[pivot_index];
+    arr[pivot_index] = arr[i];
+    arr[i] = pivot;
+    return;
+  } 
+  int temp_piv = arr[(pivot_index - 1)];
+  arr[pivot_index] = arr[i];
+  arr[pivot_index-1] = pivot;
+  arr[i] = temp_piv;
 }
 
 int* q_sort(int* arr, int length){
@@ -31,7 +36,7 @@ int* q_sort(int* arr, int length){
 
   for (int i = 0; i < boundary; i++){
     if (arr[i] >= pivot){
-      move(&arr[i], length-i);
+      move(arr, i, length, pivot, boundary);
       move_count++;
       boundary = pivot_index - move_count;
       i--;
@@ -39,23 +44,19 @@ int* q_sort(int* arr, int length){
   }
   q_sort(&arr[0], boundary);
   q_sort(&arr[boundary+1], length - boundary-1);
-
   return NULL;
-
 }
 
 int main(){
-  int arr[] = {3,3,3,3,3,3,3,3,3,3,3};
-  int length = 11;
+  int arr[] = {10,9,6,3,5,8};
+  int length = 6;
   q_sort(arr, length);
 
-  printf("####################\n");
   for (int i = 0;i < length; i++){
     printf("%d ", arr[i]);
   }
   return 0;
 }
-  // printf("pivot: %d\n", pivot);
   // printf("low: %d\n", low);
   // printf("high: %d\n", high);
   // printf("boundary: %d\n", boundary);
