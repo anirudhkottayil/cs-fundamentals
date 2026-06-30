e55ee61fce4a973ee99ba3ae4135a1d888c3faf6
Anirudh Kottayil Prince <anirudhkottayil@gmail.com>
2026-07-01 02:36:54 +1000

    feat: add binary search insertion sorting

--- algorithms/sorting/insertion_sort.c
+++ algorithms/sorting/insertion_sort.c
@@ -1,15 +1,31 @@
 #include <stdio.h>
+#include <string.h>
 
+int insertion_point(int* arr, int length, int elem){
+  int left = 0;
+  int right = length;
+  int mid = 0;
+
+  while (left <= right){
+    mid = (left + right)/2;
+    if (arr[mid] > elem){
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return left;
+}
 
 void in_sort(int* arr, int length){
   for (int i = 1; i < length; i++){
-    int j = i;
-    int temp = arr[j];
-    while ((j > 0) && (temp < arr[j-1])){
-      arr[j] = arr[j-1];
-      j--;
+    int temp = arr[i];int indx;
+    
+    if (temp < arr[i-1]){
+      indx = insertion_point(arr, i, temp);
+      memmove(arr+indx+1, arr+indx, sizeof(int) * (i - indx));
+      arr[indx] = temp;
     }
-    arr[j] = temp;
   }
 }
 
